4a97e93303e62a6ebea490817e46ad57722dd39d
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-08-24 12:07:59 +0800

    add example for doc_research cli

---
+++ examples/cli/doc_research.sh
@@ -0,0 +1,7 @@
+# Refer to: https://github.com/modelscope/ms-agent/blob/main/projects/doc_research/README.md
+
+export OPENAI_API_KEY=xxx-xxx
+export OPENAI_BASE_URL=https://api-inference.modelscope.cn/v1/
+export OPENAI_MODEL_ID=Qwen/Qwen3-235B-A22B-Instruct-2507
+
+ms-agent app --doc_research
