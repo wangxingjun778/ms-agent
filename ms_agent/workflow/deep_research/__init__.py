cc67b1f88bd4ba275b484eda72241a6a88fc2534
班扬 <xingjun.wxj@alibaba-inc.com>
2025-09-24 05:26:27 +0800

    update

--- ms_agent/workflow/deep_research/__init__.py
+++ ms_agent/workflow/deep_research/__init__.py
@@ -3,3 +3,15 @@ from .principle import (BSGMatrixPrinciple, MECEPrinciple, ParetoPrinciple,
                         ValueChainPrinciple)
 from .research_workflow import ResearchWorkflow
 from .research_workflow_beta import ResearchWorkflowBeta
+
+__all__ = [
+    'BSGMatrixPrinciple',
+    'MECEPrinciple',
+    'ParetoPrinciple',
+    'Principle',
+    'PyramidPrinciple',
+    'ResearchWorkflow',
+    'ResearchWorkflowBeta',
+    'SWOTPrinciple',
+    'ValueChainPrinciple',
+]
